d31d8715f3c3f9c09901596069c93beeab55709a
Sergey Solovyev <se.solovyev@gmail.com>
2014-08-28 13:33:51 +0200

    ConcurrentCache tests

--- lib-tests/src/test/java/org/solovyev/android/checkout/ConcurrentCacheTest.java
+++ lib-tests/src/test/java/org/solovyev/android/checkout/ConcurrentCacheTest.java
@@ -24,8 +24,17 @@ package org.solovyev.android.checkout;
 
 import org.junit.Test;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import static org.junit.Assert.*;
-import static org.junit.Assert.assertNull;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.*;
@@ -116,9 +125,9 @@ public class ConcurrentCacheTest extends CacheTestBase {
 		cache.put(k1, newEntry());
 		final Cache.Key k11 = new Cache.Key(k1.type, k1.key + "test");
 		cache.put(k11, newEntry());
-		final Cache.Key k2 =  new Cache.Key(k1.type + 1, "test");
+		final Cache.Key k2 = new Cache.Key(k1.type + 1, "test");
 		cache.put(k2, newEntry());
-		final Cache.Key k3 =  new Cache.Key(k1.type + 2, "test");
+		final Cache.Key k3 = new Cache.Key(k1.type + 2, "test");
 		cache.put(k3, newEntry());
 
 		assertNotNull(cache.get(k1));
@@ -140,7 +149,7 @@ public class ConcurrentCacheTest extends CacheTestBase {
 		cache.put(k1, newEntry());
 		final Cache.Key k2 = newKey();
 		cache.put(k2, newEntry());
-		final  Cache.Key k3 = newKey();
+		final Cache.Key k3 = newKey();
 		cache.put(k3, newEntry());
 
 		cache.clear();
@@ -148,5 +157,99 @@ public class ConcurrentCacheTest extends CacheTestBase {
 		assertNull(cache.get(k1));
 		assertNull(cache.get(k2));
 		assertNull(cache.get(k3));
+	}
+
+	@Test
+	public void testShouldBlockOperations() throws Exception {
+		final OneThreadCache c = new OneThreadCache();
+		final ConcurrentCache cache = new ConcurrentCache(c);
+		final Executor executor = Executors.newFixedThreadPool(20);
+		final Random random = new Random(System.currentTimeMillis());
+		for(int i = 0; i < 100; i++) {
+			executor.execute(new Runnable() {
+				@Override
+				public void run() {
+					switch (random.nextInt(6)) {
+						case 0:
+							cache.get(newKey());
+							break;
+						case 1:
+							cache.put(newKey(), newEntry());
+							break;
+						case 2:
+							cache.init();
+							break;
+						case 3:
+							cache.remove(newKey());
+							break;
+						case 4:
+							cache.removeAll(newKey().type);
+							break;
+						case 5:
+							cache.clear();
+							break;
+					}
+				}
+			});
+		}
+
+		for (AssertionError exception : c.exceptions) {
+			throw exception;
+		}
+	}
+
+	private static final class OneThreadCache implements Cache {
+
+		@Nonnull
+		private final AtomicBoolean lock = new AtomicBoolean();
+
+		@Nonnull
+		private final List<AssertionError> exceptions = Collections.synchronizedList(new ArrayList<AssertionError>());
+
+		@Nullable
+		@Override
+		public Entry get(@Nonnull Key key) {
+			doOnThread();
+			return null;
+		}
+
+		private void doOnThread() {
+			if (lock.getAndSet(true)) {
+				exceptions.add(new AssertionError());
+			}
+
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				exceptions.add(new AssertionError());
+			}
+
+			lock.set(false);
+		}
+
+		@Override
+		public void put(@Nonnull Key key, @Nonnull Entry entry) {
+			doOnThread();
+		}
+
+		@Override
+		public void init() {
+			doOnThread();
+		}
+
+		@Override
+		public void remove(@Nonnull Key key) {
+			doOnThread();
+		}
+
+		@Override
+		public void removeAll(int type) {
+			doOnThread();
+		}
+
+		@Override
+		public void clear() {
+			doOnThread();
+		}
 	}
 }
