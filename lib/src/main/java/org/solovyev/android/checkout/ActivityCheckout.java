104c373563327803423f2ee93d6b31362a7e4b13
serso <se.solovyev@gmail.com>
2016-09-22 21:26:52 +0200

    Use Android code style. Part II: Manual refactor. ActivityCheckout

--- lib/src/main/java/org/solovyev/android/checkout/ActivityCheckout.java
+++ lib/src/main/java/org/solovyev/android/checkout/ActivityCheckout.java
@@ -27,31 +27,40 @@ import android.content.Intent;
 import android.util.SparseArray;
 
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
 
 import javax.annotation.Nonnull;
 
 /**
- * Recommended usage of this class with {@link android.app.Activity}:
+ * Variant of {@link Checkout} that can initiate a purchase. {@link ActivityCheckout} lives in the
+ * context of {@link Activity} as it is required by Billing API.
+ * Usage example:
  * <pre>
  * {@code
- * 	protected void onCreate(Bundle savedInstanceState) {
- * 		super.onCreate(savedInstanceState);
- * 		// ...
- * 		checkout.start();
- * 		// purchase flow is creates here as activity might be destroyed during purchase process (and
- * with it all the purchase flows are destroyed).
- * 		checkout.createPurchaseFlow(createPurchaseListener());
- * 	}
  *
- * 	protected void purchase(@Nonnull final String product, @Nonnull final String sku) {
- * 		checkout.whenReady(new Checkout.BaseListener() {
- * 			public void onReady(@Nonnull BillingRequests requests) {
- * 				requests.purchase(product, sku, null, checkout.getPurchaseFlow());
- * 			}
- * 		});
- * 	}
+ *  protected final ActivityCheckout checkout = Checkout.forActivity(this, getBilling());
+ *
+ *  protected void onCreate(Bundle savedInstanceState) {
+ *      super.onCreate(savedInstanceState);
+ *      checkout.start();
+ *  }
+ *
+ *  protected void purchase(final String product, final String sku) {
+ *      checkout.whenReady(new Checkout.ListenerAdapter() {
+ *          public void onReady(BillingRequests requests) {
+ *              requests.purchase(product, sku, null, checkout.getPurchaseFlow());
+ *          }
+ *      });
+ *  }
+ *
+ *  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+ *      super.onActivityResult(requestCode, resultCode, data);
+ *      checkout.onActivityResult(requestCode, resultCode, data);
+ *  }
+ *
+ *  protected void onDestroy() {
+ *      checkout.stop();
+ *      super.onDestroy();
+ *  }
  * }
  * </pre>
  * Another usage with "one-shot" purchase flows can be found in documentation for {@link
@@ -62,10 +71,7 @@ public final class ActivityCheckout extends Checkout {
     static final int DEFAULT_REQUEST_CODE = 0XCAFE;// mm, coffee
 
     @Nonnull
-    private final SparseArray<PurchaseFlow> purchaseFlows = new SparseArray<PurchaseFlow>();
-
-    @Nonnull
-    private final Set<Integer> oneShotPurchaseFlows = new HashSet<Integer>();
+    private final SparseArray<PurchaseFlow> mFlows = new SparseArray<PurchaseFlow>();
 
     ActivityCheckout(@Nonnull final Activity activity, @Nonnull Billing billing, @Nonnull Collection<String> products) {
         super(activity, billing, products);
@@ -73,30 +79,28 @@ public final class ActivityCheckout extends Checkout {
 
     @Override
     public void stop() {
-        oneShotPurchaseFlows.clear();
-        purchaseFlows.clear();
+        mFlows.clear();
         super.stop();
     }
 
     /**
-     * Same as {@link #createPurchaseFlow(int, RequestListener)} but with default request code
+     * Same as {@link #createPurchaseFlow(int, RequestListener)} but with the default request code
      */
     public void createPurchaseFlow(@Nonnull RequestListener<Purchase> listener) {
         createPurchaseFlow(DEFAULT_REQUEST_CODE, listener);
     }
 
     /**
-     * Creates a permanent purchase flow with purchase <var>listener</var> to wait for purchase
-     * updates. Listener will receive updates only from
-     * purchase which was started with <var>requestCode</var>.
-     * All purchase flows are automatically destroyed in {@link #stop()} method.
-     * Permanent purchase flows are not destroyed when are finished (comparing to "one-shot"
-     * purchase flows), thus,
-     * <var>listener</var> methods might be called several times if several purchases with same
-     * <var>requestCode</var>
-     * are done.
+     * Creates a permanent purchase flow with a <var>listener</var> that receives purchase
+     * updates.
+     * Listener will receive updates only from the purchase marked with given
+     * <var>requestCode</var>.
+     * All flows are automatically destroyed in {@link #stop()} method.
+     * Permanent purchase flows are not destroyed when they are finished (comparing to "one-shot"
+     * flows), thus, <var>listener</var> methods might be called several times if several purchases
+     * with the same <var>requestCode</var> were initiated.
      *
-     * @param requestCode request code associated with purchase
+     * @param requestCode request code associated with a purchase
      * @param listener    purchase listener
      */
     public void createPurchaseFlow(int requestCode, @Nonnull RequestListener<Purchase> listener) {
@@ -104,7 +108,7 @@ public final class ActivityCheckout extends Checkout {
     }
 
     /**
-     * Same as {@link #destroyPurchaseFlow(int)} but with default request code
+     * Same as {@link #destroyPurchaseFlow(int)} but with the default request code
      */
     public void destroyPurchaseFlow() {
         destroyPurchaseFlow(DEFAULT_REQUEST_CODE);
@@ -114,22 +118,21 @@ public final class ActivityCheckout extends Checkout {
      * Destroys previously created purchase flow. Nothing happens if flow has already been
      * destroyed.
      *
-     * @param requestCode request code associated with purchase
+     * @param requestCode purchase request code
      */
     public void destroyPurchaseFlow(int requestCode) {
-        final PurchaseFlow flow = purchaseFlows.get(requestCode);
-        if (flow != null) {
-            purchaseFlows.delete(requestCode);
-            oneShotPurchaseFlows.remove(requestCode);
-
-            // instead of cancelling purchase request in `Billing` class (which we can't do as we don't have `requestId`)
-            // let's just cancel it here
-            flow.cancel();
+        final PurchaseFlow flow = mFlows.get(requestCode);
+        if (flow == null) {
+            return;
         }
+        mFlows.delete(requestCode);
+        // instead of cancelling purchase request in `Billing` class (which we can't do as we don't
+        // have `requestId`) let's cancel it here
+        flow.cancel();
     }
 
     /**
-     * Same as {@link #getPurchaseFlow(int)} with default request code.
+     * Same as {@link #getPurchaseFlow(int)} with the default request code.
      */
     @Nonnull
     public PurchaseFlow getPurchaseFlow() {
@@ -137,13 +140,13 @@ public final class ActivityCheckout extends Checkout {
     }
 
     /**
-     * @param requestCode request request code associated with purchase
+     * @param requestCode request request code associated with a purchase
      * @return previously created purchase flow associated with <var>requestCode</var>
      * @throws IllegalArgumentException if purchase flow for <var>requestCode</var> doesn't exist
      */
     @Nonnull
     public PurchaseFlow getPurchaseFlow(int requestCode) {
-        final PurchaseFlow flow = purchaseFlows.get(requestCode);
+        final PurchaseFlow flow = mFlows.get(requestCode);
         if (flow == null) {
             throw new IllegalArgumentException("Purchase flow doesn't exist. Have you forgotten to create it?");
         }
@@ -151,7 +154,8 @@ public final class ActivityCheckout extends Checkout {
     }
 
     /**
-     * Same as {@link #createOneShotPurchaseFlow(int, RequestListener)} with default request code
+     * Same as {@link #createOneShotPurchaseFlow(int, RequestListener)} with the default request
+     * code.
      */
     @Nonnull
     public PurchaseFlow createOneShotPurchaseFlow(@Nonnull RequestListener<Purchase> listener) {
@@ -160,40 +164,37 @@ public final class ActivityCheckout extends Checkout {
 
     /**
      * Creates a new "one-shot" purchase flow associated with <var>requestCode</var> with purchase
-     * <var>listener</var>. As soon as purchase flow
-     * is finished it is destroyed and <var>listener</var> is unregistered. Next purchase should be
-     * done with a new purchase flow. This might
+     * <var>listener</var>. As soon as the flow is finished it is destroyed and <var>listener</var>
+     * is unregistered. Next purchase should be initiated with a new purchase flow. This might
      * be useful if activity is never destroyed - then instead of calling {@link
      * ActivityCheckout#createPurchaseFlow(int, RequestListener)} in {@link
-     * android.app.Activity#onCreate(android.os.Bundle)}
-     * and {@link ActivityCheckout#getPurchaseFlow()} while starting purchase flow only this method
-     * might be used:
+     * android.app.Activity#onCreate(android.os.Bundle)} and {@link ActivityCheckout#getPurchaseFlow()}
+     * while starting a purchase flow only this method might be used:
      * <pre>
      * {@code
-     * 	protected void onCreate(Bundle savedInstanceState) {
-     * 		super.onCreate(savedInstanceState);
-     * 		// ...
-     * 		checkout.start();
-     * 		// NOTE: we don't need to create purchase flow here, it is created in `purchase` method
-     * 	}
+     *  protected void onCreate(Bundle savedInstanceState) {
+     *      super.onCreate(savedInstanceState);
+     *      // ...
+     *      checkout.start();
+     *      // NOTE: we don't need to create purchase flow here, it is created in `purchase` method
+     *  }
      *
-     * 	protected void purchase(@Nonnull final String product, @Nonnull final String sku) {
-     * 		checkout.whenReady(new Checkout.BaseListener() {
-     * 			public void onReady(@Nonnull BillingRequests requests) {
-     * 				// listener will be unregistered when the purchase flow finishes. If this method is
-     * called with the same requestCode during
-     * 				// the purchase process exception will be raised
-     * 				requests.purchase(product, sku, null, checkout.createOneShotPurchaseFlow(createPurchaseListener()));
-     * 			}
-     * 		});
+     *  protected void purchase(final String product, final String sku) {
+     *      checkout.whenReady(new Checkout.ListenerAdapter() {
+     *          public void onReady(BillingRequests requests) {
+     *              // listener will be unregistered when the purchase flow finishes. If this method is
+     *              // called several times with the same requestCode an exception is raised
+     *              requests.purchase(product, sku, null, checkout.createOneShotPurchaseFlow(createPurchaseListener()));
+     *          }
+     *      });
      * 	}
      * }
      * </pre>
      * <p/>
-     * See {@link ActivityCheckout#createPurchaseFlow(int, RequestListener)} for adding the
+     * See {@link ActivityCheckout#createPurchaseFlow(int, RequestListener)} for creating a
      * permanent purchase flows.
      *
-     * @param requestCode request code associated with purchase
+     * @param requestCode request code associated with a purchase
      * @param listener    purchase listener
      * @return newly created "one-shot" purchase flow
      * @throws IllegalArgumentException if purchase flow for <var>requestCode</var> already exists
@@ -205,56 +206,54 @@ public final class ActivityCheckout extends Checkout {
 
     @Nonnull
     private PurchaseFlow createPurchaseFlow(final int requestCode, @Nonnull RequestListener<Purchase> listener, boolean oneShot) {
-        PurchaseFlow flow = purchaseFlows.get(requestCode);
-        if (flow == null) {
-            if (oneShot) {
-                listener = new RequestListenerWrapper<Purchase>(listener) {
-                    @Override
-                    public void onError(int response, @Nonnull Exception e) {
-                        destroyPurchaseFlow(requestCode);
-                        super.onError(response, e);
-                    }
-
-                    @Override
-                    public void onCancel() {
-                        destroyPurchaseFlow(requestCode);
-                    }
-
-                    @Override
-                    public void onSuccess(@Nonnull Purchase result) {
-                        destroyPurchaseFlow(requestCode);
-                        super.onSuccess(result);
-                    }
-                };
-            }
-            //noinspection ConstantConditions
-            flow = billing.createPurchaseFlow((Activity) context, requestCode, listener);
-            purchaseFlows.append(requestCode, flow);
-            if (oneShot) {
-                oneShotPurchaseFlows.add(requestCode);
-            }
-        } else {
+        PurchaseFlow flow = mFlows.get(requestCode);
+        if (flow != null) {
             throw new IllegalArgumentException("Purchase flow associated with requestCode=" + requestCode + " already exists");
         }
+        if (oneShot) {
+            listener = new RequestListenerWrapper<Purchase>(listener) {
+                @Override
+                public void onError(int response, @Nonnull Exception e) {
+                    destroyPurchaseFlow(requestCode);
+                    super.onError(response, e);
+                }
+
+                @Override
+                public void onCancel() {
+                    destroyPurchaseFlow(requestCode);
+                }
+
+                @Override
+                public void onSuccess(@Nonnull Purchase result) {
+                    destroyPurchaseFlow(requestCode);
+                    super.onSuccess(result);
+                }
+            };
+        }
+        flow = billing.createPurchaseFlow(getActivity(), requestCode, listener);
+        mFlows.append(requestCode, flow);
         return flow;
+    }
+
+    private Activity getActivity() {
+        return (Activity) context;
     }
 
     /**
      * When used with activity this method must be called from {@link android.app.Activity#onActivityResult(int,
      * int, android.content.Intent)}
      *
-     * @return true if activity result was handled (there exists purchase flow for
+     * @return true if activity result was handled (there existed a purchase flow for the given
      * <var>requestCode</var>)
      * @see android.app.Activity#onActivityResult(int, int, android.content.Intent)
      */
     public boolean onActivityResult(int requestCode, int resultCode, Intent data) {
-        final PurchaseFlow flow = purchaseFlows.get(requestCode);
-        if (flow != null) {
-            flow.onActivityResult(requestCode, resultCode, data);
-            return true;
-        } else {
+        final PurchaseFlow flow = mFlows.get(requestCode);
+        if (flow == null) {
             Billing.warning("Purchase flow doesn't exist for requestCode=" + requestCode + ". Have you forgotten to create it?");
             return false;
         }
+        flow.onActivityResult(requestCode, resultCode, data);
+        return true;
     }
 }
